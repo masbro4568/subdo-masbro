a9579eb9b2a8add33124729fa964058e65ab0eba
masbro4568 <nonokpatok67@gmail.com>
2025-04-27 20:44:10 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// Config
+
+$zone_id = '00087397d13a3a0439b69d4f6ba02c2a';
+$fazriganz = 'luqa4yUk8efaxDhGjfe1up05Jlm8RF4dDNKcAaHX'; //apikey 
+$domen = 'lbf.my.id';//nama domen
+$ip = '_';
+$host = 'masbro_official';// nama host
